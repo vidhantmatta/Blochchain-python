96525db24e80ffa4ca0f3b5bd0bcf696c8c0da81
mattavidhant <mattavidhant@gmail.com>
2017-11-21 03:28:56 +0530

    first commit

---
+++ toyblocks.py
@@ -0,0 +1,23 @@
+import block
+import blockchain
+
+chain = blockchain.Blockchain()
+
+data={'Aadhar':'124456783454','Salary':'120000','Propertyworth':'4500000'}
+
+
+first = block.Block(data['Aadhar'])
+second = block.Block(data['Salary'])
+third = block.Block(data['Propertyworth'])
+
+chain.add_block(first)
+chain.add_block(second)
+chain.add_block(third)
+
+first.update_data('so broke')
+
+print(chain.broken)  # True
+
+chain.repair()
+
+print(chain.broken)  # False
